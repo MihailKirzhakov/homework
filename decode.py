ffd40c51f0d7dc18f4cb92d9df7de277b2a6ccd7
Mihail Kirzhakov <kirzhakovma@mail.ru>
2024-02-28 14:05:01 +0300

    Correct the parentheses

--- decode.py
+++ decode.py
@@ -4,21 +4,21 @@ import string
 
 def decode(encode: str):
     temp_code: list[tuple] = []
-    current_num: str = ""
-    current_str: str = ""
+    current_num: str = ''
+    current_exp: str = ''
     for value in encode:
         if value in string.digits:
             current_num += value
         elif value == '[':
-            temp_code.append((int(current_num), current_str))
-            current_num = ""
-            current_str = ""
+            temp_code.append((int(current_num), current_exp))
+            current_num = ''
+            current_exp = ''
         elif value == ']':
             last_num, last_str = temp_code.pop()
-            current_str = last_str + current_str * last_num
+            current_exp = last_str + current_exp * last_num
         else:
-            current_str += value
-    return current_str
+            current_exp += value
+    return current_exp
 
 
 if __name__ == '__main__':
